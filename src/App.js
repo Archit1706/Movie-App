906077828ccd9f1d8f01bcc9582f8cdb51fe2763
Archit <architrathod77@gmail.com>
2022-12-17 12:20:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import MovieCard from './components/MovieCard'
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* <hr className="bg-gray-400 sm:mx-8 mx-2" /> */}
       <MovieCard />
     </div>
   );
